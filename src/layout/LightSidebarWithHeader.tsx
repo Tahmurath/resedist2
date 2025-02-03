dab3ebd91242c918b46b0cc5b3384fc45e99e3f0
Tahmurath <hamidreza.tahmooresi@gmail.com>
2025-02-03 18:31:07 +0330

    fix not logged in user

--- src/layout/LightSidebarWithHeader.tsx
+++ src/layout/LightSidebarWithHeader.tsx
@@ -332,7 +332,7 @@ function LightSidebarWithHeader() {
                                         />
                                         <span className="hidden lg:flex lg:items-center">
                       <span aria-hidden="true" className="ml-4 text-sm font-semibold leading-6 text-gray-900">
-                       {getUser().info.Email}
+                       {getUser()?.info?.Email}
                       </span>
                       <ChevronDownIcon aria-hidden="true" className="ml-2 h-5 w-5 text-gray-400"/>
                     </span>
