7fc8459bc177cf52a7ea8cc902afc6956bae04cd
Hamidreza <hamidreza.tahmooresi@gmail.com>
2025-03-11 12:21:02 +0330

    last changes on shadcn sidebar

--- src/layout/shadcn.tsx
+++ src/layout/shadcn.tsx
@@ -9,9 +9,11 @@ const Navlayout = () =>{
     return (
         <SidebarProvider>
           <AppSidebar />
-          <main>
+          <main className="relative flex w-full flex-1 flex-col bg-background md:peer-data-[variant=inset]:m-2 md:peer-data-[state=collapsed]:peer-data-[variant=inset]:ml-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow">
+            <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
             <SidebarTrigger />
             <Outlet />
+            </div>
           </main>
         </SidebarProvider>
       )
